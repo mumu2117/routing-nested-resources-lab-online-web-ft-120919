30816bffc64d5235123acc8f12e0854856d1bb7e
Jennifer Miller <jmillerarcher@me.com>
2020-03-19 23:59:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,13 +11,14 @@ class SongsController < ApplicationController
       @songs = Song.all
     end
   end 
+  
    def show
     @song = Song.find(params[:id])	      if params[:artist_id]
          @artist = Artist.find_by(id: params[:artist_id])
         @song = @artist.songs.find_by(id: params[:id])
         if @song.nil?
           redirect_to artist_songs_path(@artist), alert: "Song not found"
-        end
+      
       else
         @song = Song.find(params[:id])
       end
